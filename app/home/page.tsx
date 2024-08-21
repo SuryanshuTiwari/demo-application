036fce1eee3faef8fd44ea9a94c5bcce4b300cf2
SuryanshuTiwari <suryanshu.tiwari@laitkor.com>
2024-08-21 10:43:41 +0530

    initial commit

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <a href="https://example.com">Click here</a>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Home;
